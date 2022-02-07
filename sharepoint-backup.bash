bc917447e8fe02e74d8b24e702fd5e6e0bb5c8ab
ben-nida <ben.nicholson@nida.edu.au>
2022-02-07 15:42:17 +1100

    Updated script for nida live

--- sharepoint-backup.bash
+++ sharepoint-backup.bash
@@ -1,7 +1,10 @@
 #!/bin/bash/
 YL='\033[1;33m'
 now=$(date +"%Y%m%d")
-mkdir /tmp/wikibak
-tar -czvf "/tmp/wikibak/wikiname-$now.tar.gz" /var/www/html/dokuwiki
-rclone copy "/tmp/wikibak/wikiname-$now.tar.gz"  wiki2share:Backup
+mkdir /tmp/nida-live-backup
+/opt/bitnami/ctlscript.sh stop
+tar -pczvf "/tmp/nida-live-backup/NIDA-Live-$now.tar.gz" /opt/bitnami
+rclone copy "/tmp/nida-live-backup/NIDA-Live-$now.tar.gz"  nida-live-backup2:Backups/NIDA-Live
+/opt/bitnami/ctlscript.sh start
 printf "${YL} Done"
+echo
