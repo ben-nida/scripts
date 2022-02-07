2114f66e22c2332d8ce3bb0142d24ae9f06effa7
ben-nida <ben.nicholson@nida.edu.au>
2022-02-07 15:49:35 +1100

    Added Sudo

--- sharepoint-backup.bash
+++ sharepoint-backup.bash
@@ -2,9 +2,9 @@
 YL='\033[1;33m'
 now=$(date +"%Y%m%d")
 mkdir /tmp/nida-live-backup
-/opt/bitnami/ctlscript.sh stop
-tar -pczvf "/tmp/nida-live-backup/NIDA-Live-$now.tar.gz" /opt/bitnami
+sudo /opt/bitnami/ctlscript.sh stop
+sudo tar -pczvf "/tmp/nida-live-backup/NIDA-Live-$now.tar.gz" /opt/bitnami
 rclone copy "/tmp/nida-live-backup/NIDA-Live-$now.tar.gz"  nida-live-backup2:Backups/NIDA-Live
-/opt/bitnami/ctlscript.sh start
+sudo /opt/bitnami/ctlscript.sh start
 printf "${YL} Done"
 echo
