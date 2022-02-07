22d9a67bee5755a16f0531588674085fb5d619a4
ben-nida <ben.nicholson@nida.edu.au>
2022-02-07 15:56:53 +1100

    Added NC for Done, and cleaned up tmp

--- sharepoint-backup.bash
+++ sharepoint-backup.bash
@@ -1,10 +1,12 @@
 #!/bin/bash
 YL='\033[1;33m'
+NC='\033[0m' # No Color
 now=$(date +"%Y%m%d")
 mkdir /tmp/nida-live-backup
 sudo /opt/bitnami/ctlscript.sh stop
 sudo tar -pczvf "/tmp/nida-live-backup/NIDA-Live-$now.tar.gz" /opt/bitnami
 rclone copy "/tmp/nida-live-backup/NIDA-Live-$now.tar.gz"  nida-live-backup2:Backups/NIDA-Live
 sudo /opt/bitnami/ctlscript.sh start
-printf "${YL} Done"
+rm -r /tmp/nida-live-backup
+printf "${YL}Done${NC}"
 echo
